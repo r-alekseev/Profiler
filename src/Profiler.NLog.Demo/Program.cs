9dc5ed5691cfe7c934c1c738ff633a54c3d2de36
Roman Alekseev <r-alekseev@tuta.io>
2018-12-24 02:13:10 +0400

    trace to nlog - project and class implements trace using NLog - demo NLog console application

---
+++ src/Profiler.NLog.Demo/Program.cs
@@ -0,0 +1,46 @@
+﻿using NLog;
+using System;
+using System.Threading;
+
+namespace Profiler.NLog.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager.LoadConfiguration("nlog.config");
+
+            var logger = LogManager.GetCurrentClassLogger();
+
+            var profiler = new SectionProvider(
+                getTimeMeasure: () => new StopwatchTimeMeasure(),
+                measureTrace: new NLogMeasureTrace(logger));
+
+            DoWork(profiler);
+
+            LogManager.Shutdown();
+            Console.WriteLine("Press any key to exit ..");
+            Console.ReadKey();
+        }
+
+        private static void DoWork(ISectionProvider profiler)
+        {
+            using (profiler.Section("dowork"))
+            {
+                Thread.Sleep(123);
+
+                DoPartOfWork(profiler);
+
+                Thread.Sleep(123);
+            }
+        }
+
+        private static void DoPartOfWork(ISectionProvider profiler)
+        {
+            using (profiler.Section("dopartofwork"))
+            {
+                Thread.Sleep(321);
+            }
+        }
+    }
+}
---
+++ src/Profiler.NLog/NLogMeasureTrace.cs
@@ -0,0 +1,31 @@
+﻿using NLog;
+using System;
+
+namespace Profiler
+{
+    public class NLogMeasureTrace : IMeasureTrace
+    {
+        private readonly ILogger _logger;
+        private readonly LogLevel _logLevel;
+        private readonly string _timeSpanFormat;
+
+        public NLogMeasureTrace(ILogger logger, LogLevel logLevel, string timeSpanFormat)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _logLevel = logLevel ?? throw new ArgumentNullException(nameof(logLevel));
+
+            _timeSpanFormat = timeSpanFormat;
+        }
+
+        public NLogMeasureTrace(ILogger logger, string timeSpanFormat = "g")
+            : this(logger, LogLevel.Trace, timeSpanFormat)
+        {
+        }
+
+        public void Log(TimeSpan elapsed, string format, params object[] args)
+        {
+            string message = $"[{elapsed.ToString(_timeSpanFormat)}] {string.Format(format, args)}";
+            _logger.Log(_logLevel, message);
+        }
+    }
+}
