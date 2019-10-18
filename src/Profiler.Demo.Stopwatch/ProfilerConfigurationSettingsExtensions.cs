80475927216fe50a28e4db2af6521a53b54fd67c
Roman Alekseev <r-alekseev@tuta.io>
2019-10-19 00:01:08 +0300

    configuration chaining in demo console app

--- src/Profiler.Demo.Stopwatch/ProfilerConfigurationSettingsExtensions.cs
+++ src/Profiler.Demo.Stopwatch/ProfilerConfigurationSettingsExtensions.cs
@@ -4,14 +4,16 @@ namespace Profiler.Demo.Stopwatch
 {
     public static class ProfilerConfigurationSettingsExtensions
     {
-        public static void UseConsoleTraceWriter(this IProfilerConfiguration settings)
+        public static IProfilerConfiguration UseConsoleTraceWriter(this IProfilerConfiguration settings)
         {
             settings.CreateTraceWriter = () => new ConsoleTraceWriter();
+            return settings;
         }
 
-        public static void UseConsoleReportWriter(this IProfilerConfiguration settings)
+        public static IProfilerConfiguration UseConsoleReportWriter(this IProfilerConfiguration settings)
         {
             settings.CreateReportWriter = () => new ConsoleReportWriter();
+            return settings;
         }
     }
 }
