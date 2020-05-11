b18eb95648de00721cbad894942df65d545c4ef0
Roman Alekseev <r-alekseev@tuta.io>
2020-05-11 03:28:58 +0300

    easier configuration

--- src/Profiler.Demo.Stopwatch/ProfilerConfigurationSettingsExtensions.cs
+++ src/Profiler.Demo.Stopwatch/ProfilerConfigurationSettingsExtensions.cs
@@ -4,16 +4,10 @@ namespace Profiler.Demo.Stopwatch
 {
     public static class ProfilerConfigurationSettingsExtensions
     {
-        public static IProfilerConfiguration UseConsoleTraceWriter(this IProfilerConfiguration settings)
-        {
-            settings.CreateTraceWriter = () => new ConsoleTraceWriter();
-            return settings;
-        }
+        public static IProfilerConfiguration UseConsoleTraceWriter(this IProfilerConfiguration settings) => settings
+            .UseTraceWriter(create: () => new ConsoleTraceWriter());
 
-        public static IProfilerConfiguration UseConsoleReportWriter(this IProfilerConfiguration settings)
-        {
-            settings.CreateReportWriter = () => new ConsoleReportWriter();
-            return settings;
-        }
+        public static IProfilerConfiguration UseConsoleReportWriter(this IProfilerConfiguration settings) => settings
+            .UseReportWriter(create: () => new ConsoleReportWriter());
     }
 }
--- src/Profiler/Implementations/DebugTraceWriter.cs
+++ src/Profiler/Implementations/DebugTraceWriter.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Profiler
 {
     public class DebugTraceWriter : ITraceWriter
     {
+        private static readonly Lazy<DebugTraceWriter> _lazy =
+            new Lazy<DebugTraceWriter>(
+                valueFactory: () => new DebugTraceWriter(),
+                mode: LazyThreadSafetyMode.ExecutionAndPublication);
+
+        public static DebugTraceWriter Instance => _lazy.Value;
+
+        private DebugTraceWriter() { }
+
         public void Write(TimeSpan elapsed, string[] chain, params object[] args)
         {
             Debug.Write($"[{elapsed.TotalMilliseconds} ms] {string.Join(" -> ", chain)}");
